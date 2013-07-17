f209b5796a307bdc9490734f10c27cdd86c89697
pawelrychlik <pawel.rychlik@rspective.pl>
2013-07-17 19:33:14 +0200

    refined logging in quality measurer for convenient searching

--- quality_measurer.rb
+++ quality_measurer.rb
@@ -29,13 +29,13 @@ class QualityMeasurer
       status = facet['count'] == 1
       if status then ok += 1 else fail += 1 end
 
-      @log.info "#{status ? 'OK' : 'FAIL'}: group_id=#{facet['term']} assigned to  #{facet['count']} orgs."
+      @log.info "#{status ? 'OK' : 'FAIL'}: \"group_id\"=>\"#{facet['term']}\" assigned to  #{facet['count']} orgs."
     }
 
     results.results.each { |org|
       status = org['group_id'].uniq.length == 1
       error += 1 unless status
-      @log.info "ERROR: Org[es_id=#{org['es_id']}] has more than one group_id=#{org['group_id']}." unless status
+      @log.info "ERROR: Org[\"es_id\"=>#{org['es_id']}] has more than one \"group_id\"=>#{org['group_id']}." unless status
     }
 
     @log.info "Duplicate resolution: OK=#{ok}, FAIL=#{fail}, ERROR=#{error}."
