5e48b1886b18d464a16d31a7156c7dbb2670fdbd
pawelrychlik <pawel.rychlik@rspective.pl>
2013-07-17 16:58:07 +0200

    refined quality measures

--- quality_measurer.rb
+++ quality_measurer.rb
@@ -8,14 +8,36 @@ class QualityMeasurer
   def measure
     ok = 0
     fail = 0
+    error = 0
 
-    @client.all.results.each { |org|
-      status = org['group_id'].uniq.length == 1
-      if status then ok += 1 else fail += 1 end
-
-      @log.info "#{status ? 'OK' : 'FAIL'}: Org[es_id=#{org['es_id']}; group_id=#{org['group_id']}]"
+    query = {
+        query: { match_all: {} },
+        size: 1000000,
+        facets: {
+            group_id: {
+                terms: {
+                    field: 'group_id',
+                    size: 1000000
+                }
+            }
+        }
     }
 
-    @log.info "Duplicate resolution: OK=#{ok}, FAIL=#{fail}"
+    results = @client.search(query)
+
+    results.facets.group_id.terms.each { |facet|
+      status = facet['count'] == 1
+      if status then ok += 1 else fail += 1 end
+
+      @log.info "#{status ? 'OK' : 'FAIL'}: group_id=#{facet['term']} assigned to  #{facet['count']} orgs."
+    }
+
+    results.results.each { |org|
+      status = org['group_id'].uniq.length == 1
+      error += 1 unless status
+      @log.info "ERROR: Org[es_id=#{org['es_id']}] has more than one group_id=#{org['group_id']}." unless status
+    }
+
+    @log.info "Duplicate resolution: OK=#{ok}, FAIL=#{fail}, ERROR=#{error}."
   end
 end
